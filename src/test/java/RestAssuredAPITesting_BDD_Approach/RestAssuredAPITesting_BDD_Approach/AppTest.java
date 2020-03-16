5f6401248a6d9f8512a5d2717535b530e74db894
easXin <codingericwang@gmail.com>
2020-03-15 19:56:05 -0700

    init commit

---
+++ src/test/java/RestAssuredAPITesting_BDD_Approach/RestAssuredAPITesting_BDD_Approach/AppTest.java
@@ -0,0 +1,20 @@
+package RestAssuredAPITesting_BDD_Approach.RestAssuredAPITesting_BDD_Approach;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
